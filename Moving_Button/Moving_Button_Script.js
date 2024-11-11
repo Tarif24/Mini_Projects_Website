e81adbe09021a7650f62f44ed53007d3c2f60087
Tarif24 <tarif24@hotmail.com>
2024-11-11 18:09:01 -0500

    fixed button going off screen and added diffrent responses based on no button hover counter

--- Moving_Button/Moving_Button_Script.js
+++ Moving_Button/Moving_Button_Script.js
@@ -1,10 +1,34 @@
 const noBtn = document.getElementById("no-btn");
+const yesBtn = document.getElementById("yes-btn");
 const mainContainer = document.getElementById("question-app");
 
 const containerRect = mainContainer.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 
+let noCtr = 0;
+
 noBtn.addEventListener('mouseover', OnNoButtonHover);
+yesBtn.addEventListener('click', () => 
+{
+    
+    if (noCtr == 0)
+    {
+        alert("ALR THEN HOP ON");
+    }
+    else if (noCtr <= 5)
+    {
+        alert("WHY DONT YOU WANNA PLAY WITH ME");
+    }
+    else if (noCtr <= 10)
+    {
+        alert("TOOK YOU LONG ENOUGH TO SAY YES");
+    }
+    else
+    {
+        alert("WHY DID IT TAKE SO LONG TO SAY YES");
+    } 
+
+})
 
 function OnNoButtonHover()
 {
@@ -16,4 +40,6 @@ function OnNoButtonHover()
 
     noBtn.style.left = x + 'px';
     noBtn.style.top = y + 'px';
+
+    noCtr++;
 }
